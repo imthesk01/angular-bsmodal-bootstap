67f4b1afe4b33df6b52d0ba4e68187b8e2d672c9
imthesk01 <64018806+imthesk01@users.noreply.github.com>
2023-04-21 09:16:26 +0530

    4 files modified, 3 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { ModelCompComponent } from './model-comp/model-comp.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ModalModule.forRoot() ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ModelCompComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/model-comp/model-comp.component.ts
+++ app/model-comp/model-comp.component.ts
@@ -1,14 +1,13 @@
-import { Component, TemplateRef, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { Observable } from 'rxjs';
+
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: 'app-model-comp',
+  templateUrl: './model-comp.component.html',
+  styleUrls: ['./model-comp.component.css'],
 })
-export class AppComponent implements OnInit {
-  name = 'Angular 5';
+export class ModelCompComponent implements OnInit {
   modalRef: BsModalRef;
   constructor(
     public modalService: BsModalService,
@@ -18,15 +17,6 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.modalService.onHide.subscribe((e) => {
       console.log('close', this.modalService.config.initialState);
-    });
-  }
-
-  openModal(template: TemplateRef<any>) {
-    const user = {
-      id: 10,
-    };
-    this.modalRef = this.modalService.show(template, {
-      initialState: user,
     });
   }
 
